7baf9c14253de1dcd78ae48c254e8e9bc317cea4
Jashanpreet Singh <128256855+Jashanpreet2@users.noreply.github.com>
2026-07-20 10:14:59 +0530

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last = dict()
+        res = []
+        for i, c in enumerate(s):
+            last[c] = i
+        remaining = set(s[0])
+        length = 0
+        for i, c in enumerate(s):
+            remaining.add(c)
+            length += 1
+            if i == last[c]:
+                remaining.remove(c)
+            if len(remaining)==0:
+                res.append(length)
+                length = 0
+        return res
+
+
+
+            
+
+        
