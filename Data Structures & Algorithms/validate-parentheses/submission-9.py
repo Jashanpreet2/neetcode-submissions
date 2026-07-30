aceea7646c90ba44279ff6c418a5f7726322209f
Jashanpreet Singh <128256855+Jashanpreet2@users.noreply.github.com>
2026-07-30 20:17:32 +0530

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        ctoo = {')':'(', '}':'{', ']':'['}
+        stack = []
+        for b in s:
+            if b in ctoo:
+                if len(stack) == 0 or ctoo[b] != stack[-1]:
+                    return False
+                stack.pop()
+                continue
+            stack.append(b)
+        return len(stack) == 0
+        
